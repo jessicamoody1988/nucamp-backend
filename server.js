dd17b8b715c0de6c06b8dfeef74e1480aabbb3ad
Jessica Moody <jessicamoody1988@gmail.com>
2021-08-26 16:01:10 -0500

    Exercise: Express Router part 1

--- server.js
+++ server.js
@@ -11,6 +11,64 @@ const app = express();
 // This configures morgan to log using the dev ver
 app.use(morgan('dev'));
 
+// When the server receives requests in json format, this will handle the parsing
+// of the json data into JS properties of the request object
+app.use(express.json());
+
+// Set default properties for all routing methods for path /campsites
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    // Next passes control of the app routing to the next relevent routing method after this one
+    next();
+});
+
+// Endpoint for GET requests for path /campsites
+// res.statusCode and res.setHeader is already set by call to app.all
+app.get('/campsites', (req, res) => {
+    res.end('Will send all the campsites to you');
+});
+
+// Endpoint for POST requests for path /campsites
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+// Endpoint for PUT requests for path /campsites
+// We are rejecting PUT requests as not-supported 
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+})
+
+// Endpoint for DELETE requests for path /campsites
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+// Endpoint for GET requests for path /campsites/:campsiteId
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+});
+
+// Endpoint for POST requests for path /campsites/:campsiteId
+// We are rejecting POST requests as not-supported
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+// Endpoint for PUT requests for path /campsites/:campsiteId
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+// Endpoint for DELETE requests for path /campsites/:campsiteId
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
 // Setup express to serve files from the public folder 
 app.use(express.static(__dirname + '/public'));
 
