358281e9b0148e1e3b1306414302e9a3f51bfde9
Jessica Moody <jessicamoody1988@gmail.com>
2021-08-28 09:17:08 -0500

    Week 1 Workshop Assignment Task 1

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -3,7 +3,7 @@ const express = require('express');
 // Setup a new express router 
 const campsiteRouter = express.Router();
 
-// Path is set in server.js
+// Path is passed in from server.js
 campsiteRouter.route('/')
 
 // Set default properties for all routing methods for path /campsites
@@ -35,6 +35,38 @@ campsiteRouter.route('/')
 // Endpoint for DELETE requests for path /campsites
 .delete((req, res) => {
     res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+
+// Set default properties for all routing methods for path /campsites/:campsiteId
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+// Endpoint for GET requests for path /campsites/:campsiteId
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+
+// Endpoint for POST requests for path /campsites/:campsiteId
+// We are rejecting POST requests as not-supported
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+
+// Endpoint for PUT requests for path /campsites/:campsiteId 
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+
+// Endpoint for DELETE requests for path /campsites/:campsiteId
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
 });
 
 module.exports = campsiteRouter;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const campsiteRouter = require('./routes/campsiteRouter');
+
 // server variables
 const hostname = 'localhost';
 const port = 3000;
@@ -15,59 +17,8 @@ app.use(morgan('dev'));
 // of the json data into JS properties of the request object
 app.use(express.json());
 
-// Set default properties for all routing methods for path /campsites
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    // Next passes control of the app routing to the next relevent routing method after this one
-    next();
-});
-
-// Endpoint for GET requests for path /campsites
-// res.statusCode and res.setHeader is already set by call to app.all
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
-
-// Endpoint for POST requests for path /campsites
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-// Endpoint for PUT requests for path /campsites
-// We are rejecting PUT requests as not-supported 
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-})
-
-// Endpoint for DELETE requests for path /campsites
-app.delete('/campsites', (req, res) => {
-    res.end('Deleting all campsites');
-});
-
-// Endpoint for GET requests for path /campsites/:campsiteId
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
-
-// Endpoint for POST requests for path /campsites/:campsiteId
-// We are rejecting POST requests as not-supported
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
-
-// Endpoint for PUT requests for path /campsites/:campsiteId
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
-
-// Endpoint for DELETE requests for path /campsites/:campsiteId
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+// Provide the root path for the campsiteRouter
+app.use('/campsites', campsiteRouter);
 
 // Setup express to serve files from the public folder 
 app.use(express.static(__dirname + '/public'));
